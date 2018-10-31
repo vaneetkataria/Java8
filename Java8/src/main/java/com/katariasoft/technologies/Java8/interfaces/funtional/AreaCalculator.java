38bfd2ac4a0e8a49fbfe511a3860ddb0e9a0c4ba
vaneet <vaneetkataria54@gmail.com>
2018-10-31 18:14:31 +0530

    committed test cases for lambdas and funtional interfaces

--- Java8/src/main/java/com/katariasoft/technologies/Java8/interfaces/funtional/AreaCalculator.java
+++ Java8/src/main/java/com/katariasoft/technologies/Java8/interfaces/funtional/AreaCalculator.java
@@ -2,5 +2,7 @@ package com.katariasoft.technologies.Java8.interfaces.funtional;
 
 public interface AreaCalculator {
 	public int getArea(int length, int breadth);
+	// as soon as this method is added compiler will start complaining.
+	// public int getParemeter(int length, int breadth);
 
 }
