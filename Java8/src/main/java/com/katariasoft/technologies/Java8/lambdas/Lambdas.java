37fd79f08f54cfbfa88549880b8be43d0a7e6957
vaneet <vaneetkataria54@gmail.com>
2018-10-31 17:52:31 +0530

    committed test cases for lambdas and funtional interfaces

--- Java8/src/main/java/com/katariasoft/technologies/Java8/lambdas/Lambdas.java
+++ Java8/src/main/java/com/katariasoft/technologies/Java8/lambdas/Lambdas.java
@@ -1,6 +1,14 @@
 package com.katariasoft.technologies.Java8.lambdas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import com.katariasoft.technologies.Java8.interfaces.funtional.AreaCalculator;
 import com.katariasoft.technologies.Java8.interfaces.funtional.BaseFunctionalInterface;
@@ -14,12 +22,15 @@ public class Lambdas {
 		String PRINT_RECTANGLE_AREA = "PRINT_RECTANGLE_AREA";
 		String CHILD_THREAD_RUN = "CHILD_THREAD_RUN";
 		String NON_FUNCTIONAL_INTERFACE = "NON_FUNCTIONAL_INTERFACE";
+		String SORT_AN_ARRAYLIST = "SORT_AN_ARRAYLIST";
+		String TREE_SET_CASE = "TREE_SET_CASE";
+		String TREE_MAP_CASE = "TREE_MAP_CASE";
 
 	}
 
 	public static void main(String args[]) {
 		Lambdas lambdas = new Lambdas();
-		lambdas.executeCase(LambdaTestCases.NON_FUNCTIONAL_INTERFACE);
+		lambdas.executeCase(LambdaTestCases.TREE_MAP_CASE);
 	}
 
 	public void executeCase(String testCase) {
@@ -38,6 +49,15 @@ public class Lambdas {
 			break;
 		case LambdaTestCases.NON_FUNCTIONAL_INTERFACE:
 			nonFunctionalIntreface();
+			break;
+		case LambdaTestCases.SORT_AN_ARRAYLIST:
+			sortAnArrayList();
+			break;
+		case LambdaTestCases.TREE_SET_CASE:
+			treeSetCase();
+			break;
+		case LambdaTestCases.TREE_MAP_CASE:
+			treeMapCase();
 			break;
 
 		default:
@@ -116,6 +136,83 @@ public class Lambdas {
 	private void nonFunctionalIntreface() {
 		// NonFunctionalInterface nfi = (int i) -> System.out.println(i);
 		// nfi.input(5);
+	}
+
+	private void sortAnArrayList() {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(5);
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		System.out.println("List is: " + list);
+		Collections.sort(list);
+		System.out.println("Sorted List is: " + list);
+		// using lambdas to descending sorting.
+		Comparator<Integer> comparator = (Integer a, Integer b) -> {
+			return (a < b) ? 1 : (a > b) ? -1 : 0;
+		};
+		// using lambdas to descending sorting.
+		Comparator<Integer> comparatorMoreConcise = (a, b) -> (a < b) ? 1 : (a > b) ? -1 : 0;
+		Collections.sort(list, comparatorMoreConcise);
+		System.out.println("Sorted List in descending order is: " + list);
+
+	}
+
+	private void treeSetCase() {
+		Set<Integer> list = new TreeSet<>();
+		list.add(1);
+		list.add(5);
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(1);
+		list.add(2);
+		System.out.println("Tree Set is: " + list);
+
+		// Provide descending sort logic to constructor with lambdas
+
+		Set<Integer> set = new TreeSet<>((a, b) -> a < b ? 1 : a > b ? -1 : 0);
+		set.add(1);
+		set.add(5);
+		set.add(4);
+		set.add(3);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		set.add(1);
+		set.add(2);
+		System.out.println("Descending  sorted Tree Set is: " + set);
+
+	}
+
+	private void treeMapCase() {
+		Map<Integer, String> map = new TreeMap<>();
+		map.put(6, "6");
+		map.put(7, "7");
+		map.put(8, "8");
+		map.put(3, "3");
+		map.put(1, "1");
+		map.put(2, "2");
+		map.put(4, "4");
+		map.put(5, "5");
+		System.out.println("Map is : " + map);
+
+		Map<Integer, String> mapDesc = new TreeMap<>((a, b) -> a > b ? -1 : a < b ? 1 : 0);
+		mapDesc.put(6, "6");
+		mapDesc.put(7, "7");
+		mapDesc.put(8, "8");
+		mapDesc.put(3, "3");
+		mapDesc.put(1, "1");
+		mapDesc.put(2, "2");
+		mapDesc.put(4, "4");
+		mapDesc.put(5, "5");
+		System.out.println("Descending Map is : " + mapDesc);
+
 	}
 
 }
