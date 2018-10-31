bf5f586aff2cc1f368b1c98c0746252599549c22
vaneet <vaneetkataria54@gmail.com>
2018-10-31 16:16:17 +0530

    Added initial java8 features project

---
+++ Java8/src/main/java/com/katariasoft/technologies/Java8/App.java
@@ -0,0 +1,13 @@
+package com.katariasoft.technologies.Java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
