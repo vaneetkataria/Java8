523d9fab13fa851be4d1658f709a88297d34dc8c
vaneet <vaneetkataria54@gmail.com>
2018-11-03 19:41:09 +0530

    committed chnages for bifunctions

---
+++ Java8/src/main/java/com/katariasoft/technologies/Java8/interfaces/funtional/predefined/supplier/OtpSupplierProvider.java
@@ -0,0 +1,27 @@
+package com.katariasoft.technologies.Java8.interfaces.funtional.predefined.supplier;
+
+import java.util.function.Supplier;
+
+public class OtpSupplierProvider {
+
+	private static final String empty = "";
+
+	public Supplier<String> eightDigitOtpProvider() {
+		return () -> {
+			byte[] allNumericDigits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			String otp = "";
+			for (byte b = 0; b < 8; b++) {
+				byte randomDigit = allNumericDigits[(byte) (Math.random() * 10)];
+				otp = String.join(empty, otp, String.valueOf(randomDigit));
+			}
+			return otp;
+		};
+	}
+
+	public static void main(String args[]) {
+		OtpSupplierProvider otpSupplierProvider = new OtpSupplierProvider();
+		for (byte b = 0; b < 20; b++)
+			System.out.println("OTP generated is :" + otpSupplierProvider.eightDigitOtpProvider().get());
+	}
+
+}
