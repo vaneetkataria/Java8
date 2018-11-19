cb1a92213ac6d5d80c900970185a7fc851bc1db7
vaneet <vaneetkataria54@gmail.com>
2018-11-19 18:34:55 +0530

    Added code parallel streams

--- Java8/src/main/java/com/katariasoft/technologies/Java8/jodatime/JodaTimeUseCases.java
+++ Java8/src/main/java/com/katariasoft/technologies/Java8/jodatime/JodaTimeUseCases.java
@@ -2,4 +2,28 @@ package com.katariasoft.technologies.Java8.jodatime;
 
 public class JodaTimeUseCases {
 
+	private static final String localDate = "localDate";
+	private static final String localTime = "localTime";
+	private static final String localDateTime = "localDateTime";
+
+	public static void main(String args[]) {
+
+		String testCase = "localDate";
+		switch (testCase) {
+		case localDate:
+			break;
+		case localTime:
+			break;
+		case localDateTime:
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public static void localDateUseCases() {
+
+	}
+
 }
